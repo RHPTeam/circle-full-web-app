8856772f6e6985ebfa76e80e1fd8c8f7c9dfa903
trantoan960 <trantoan.fox.97@gmail.com>
2018-12-14 22:17:59 +0700

    Update file setup router home, product

---
+++ circlek/src/components/Home/index.vue
@@ -0,0 +1,12 @@
+<template>
+	<div>
+		<h1>Homepage</h1>
+		<router-link tag="a" class="btn" :to="{ name: 'products' }"
+			>Quen me no mat</router-link
+		>
+	</div>
+</template>
+
+<script></script>
+
+<style></style>
